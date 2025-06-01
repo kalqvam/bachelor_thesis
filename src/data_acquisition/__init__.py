bde48623ac779c2a9a50c06daaa4c8021c5cc771
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-06-01 16:47:11 +0300

    Update __init__.py

--- src/data_acquisition/__init__.py
+++ src/data_acquisition/__init__.py
@@ -1,1 +1,67 @@
+from .api_client import ApiManager
+from .financial_data import (
+    filter_by_year,
+    merge_financial_data,
+    get_financial_data_for_ticker,
+    get_financial_data_parallel,
+    validate_financial_data,
+    print_financial_data_summary
+)
+from .esg_data import (
+    collect_esg_data,
+    collect_esg_risk_data,
+    convert_esg_to_dataframes,
+    validate_esg_data,
+    print_esg_data_summary,
+    get_esg_data_for_ticker,
+    analyze_esg_missing_patterns
+)
+from .profile_data import (
+    collect_company_profiles,
+    calculate_company_age,
+    get_profile_data_for_ticker,
+    validate_profile_data,
+    analyze_sector_distribution,
+    analyze_company_ages,
+    print_profile_data_summary
+)
+from .etf_holdings import (
+    get_etf_holdings,
+    validate_etf_holdings,
+    extract_tickers_from_holdings,
+    analyze_holdings_composition,
+    print_etf_holdings_summary,
+    filter_holdings_by_weight,
+    get_top_holdings
+)
 
+__all__ = [
+    'ApiManager',
+    'filter_by_year',
+    'merge_financial_data',
+    'get_financial_data_for_ticker',
+    'get_financial_data_parallel',
+    'validate_financial_data',
+    'print_financial_data_summary',
+    'collect_esg_data',
+    'collect_esg_risk_data',
+    'convert_esg_to_dataframes',
+    'validate_esg_data',
+    'print_esg_data_summary',
+    'get_esg_data_for_ticker',
+    'analyze_esg_missing_patterns',
+    'collect_company_profiles',
+    'calculate_company_age',
+    'get_profile_data_for_ticker',
+    'validate_profile_data',
+    'analyze_sector_distribution',
+    'analyze_company_ages',
+    'print_profile_data_summary',
+    'get_etf_holdings',
+    'validate_etf_holdings',
+    'extract_tickers_from_holdings',
+    'analyze_holdings_composition',
+    'print_etf_holdings_summary',
+    'filter_holdings_by_weight',
+    'get_top_holdings'
+]
