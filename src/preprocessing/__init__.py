aab3f451083f6ce831035daed3b8d468e8470547
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-06-01 20:36:20 +0300

    Update __init__.py

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,13 +1,16 @@
 from . import cleaning
+from . import declowning
 from .cleaning import *
+from .declowning import *
 from .interpolation import (
     find_consecutive_zeros,
     process_financial_data_interpolation
 )
 
 __all__ = [
-    'cleaning'
-] + cleaning.__all__ + [
+    'cleaning',
+    'declowning'
+] + cleaning.__all__ + declowning.__all__ + [
     'find_consecutive_zeros',
     'process_financial_data_interpolation'
 ]
