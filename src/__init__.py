b0b82ab4073f6fdd77ef8f234d18ccde98b025db
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-06-01 18:12:47 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,12 @@
 from . import utils
 from . import data_acquisition
+from . import preprocessing
 
 __version__ = "0.1.0"
 __author__ = "kalqvam"
 
 __all__ = [
     'utils',
-    'data_acquisition'
+    'data_acquisition',
+    'preprocessing'
 ]
