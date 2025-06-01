b1454069af35c69eefbad7361dadd6534f544dce
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-06-01 21:19:11 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from . import utils
 from . import data_acquisition
 from . import preprocessing
+from . import kalman
 
 __version__ = "0.1.0"
 __author__ = "kalqvam"
@@ -8,5 +9,6 @@ __author__ = "kalqvam"
 __all__ = [
     'utils',
     'data_acquisition',
-    'preprocessing'
+    'preprocessing',
+    'kalman'
 ]
