a04e9d42e36a4ecea9e1310c7e2cc5eaa306062d
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-06-01 18:51:32 +0300

    Update __init__.py

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,6 +1,9 @@
 from . import cleaning
+from . import interpolation
 from .cleaning import *
+from .interpolation import *
 
 __all__ = [
-    'cleaning'
-] + cleaning.__all__
+    'cleaning',
+    'interpolation'
+] + cleaning.__all__ + interpolation.__all__
