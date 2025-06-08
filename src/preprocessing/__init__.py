dec43405990c4ff5e6167732a02465c1189bc046
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-06-08 17:01:19 +0300

    Update __init__.py

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -6,11 +6,17 @@ from .interpolation import (
     find_consecutive_zeros,
     process_financial_data_interpolation
 )
+from .dummyfication import (
+    add_shock_dummy,
+    add_time_dummy
+)
 
 __all__ = [
     'cleaning',
     'declowning'
 ] + cleaning.__all__ + declowning.__all__ + [
     'find_consecutive_zeros',
-    'process_financial_data_interpolation'
+    'process_financial_data_interpolation',
+    'add_shock_dummy',
+    'add_time_dummy'
 ]
